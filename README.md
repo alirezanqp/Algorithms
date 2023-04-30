f58481608913dd57d58be82d9c0a50253d389cd2
alirezanqp <alirezanqp@gmail.com>
2023-04-30 19:57:13 +0330

    add binary search example with golang

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # algorithms
+    - binary search
---
+++ binary_search/golang/binarySearch.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func binarySearchIterative(list []int, item int) int {
+	low := 0
+	high := len(list) - 1
+
+	for low <= high {
+
+		mid := (low + high) / 2
+		guess := list[mid]
+
+		if guess == item {
+			return mid
+		}
+
+		if guess < item {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
+
+func binarySearchRecursive(list []int, item int, low int, high int) int {
+    if low > high {
+        return -1
+    }
+
+    mid := (low + high) / 2
+    guess := list[mid]
+
+    if guess == item {
+        return mid
+    } else if guess < item {
+        return binarySearchRecursive(list, item, mid+1, high)
+    } else {
+        return binarySearchRecursive(list, item, low, mid-1)
+    }
+}
+
+func main() {
+	fmt.Println(binarySearchIterative([]int{1, 2, 3, 4, 5}, 3))
+	fmt.Println(binarySearchIterative([]int{1, 2, 3, 4, 5}, 6))
+
+	fmt.Println(binarySearchRecursive([]int{1, 2, 3, 4, 5}, 3, 0, 4))
+    fmt.Println(binarySearchRecursive([]int{1, 2, 3, 4, 5}, 6, 0, 4))
+}
